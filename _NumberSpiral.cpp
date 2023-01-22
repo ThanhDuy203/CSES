146be8371607c412afd82583f28b718b07b45207
Hồ Thanh Duy <115147659+ThanhDuy203@users.noreply.github.com>
2023-01-22 23:08:26 +0700

    Add files via upload

---
+++ _NumberSpiral.cpp
@@ -0,0 +1,18 @@
+//Number Spiral
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+	//Grid
+	int x[4] = {-1, 0, 0, 1};	//hang
+	int y[4] = { 0,-1, 1, 0};	//cot
+	
+	//Oup
+	int t; cin >> t;
+	while (t--){
+		int row, col; cin >> row >> col;
+		cout << arr[row-1]col-1] <<"\n";
+	}
+	return 0;
+}
+
